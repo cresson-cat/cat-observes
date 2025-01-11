eb423dacccf1f44e18afe4b1a122b7ff07f86981
aider <kanegadai@Dai-MacBook-Air.local>
2025-01-11 17:13:29 +0900

    feat: swagger 追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // @note swagger 設定
+  const config = new DocumentBuilder()
+    .setTitle('Cat Observables')
+    .setDescription('The cat API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/models/dto/alarm-clock.model.dto.ts
+++ src/models/dto/alarm-clock.model.dto.ts
@@ -1,6 +1,7 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsNotEmpty } from 'class-validator';
-
 export class AlarmClockDto {
+  @ApiProperty({ type: [String], description: 'ファイル名の配列' })
   @IsNotEmpty()
   @IsArray()
   files: string[];
