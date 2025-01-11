bd1af0f0d928595b435c958e2c7fc7c9918a82a7
aider <kanegadai@Dai-MacBook-Air.local>
2025-01-11 17:13:29 +0900

    feat: swagger 追加

--- src/cat-in-ambush/cat-in-ambush.controller.ts
+++ src/cat-in-ambush/cat-in-ambush.controller.ts
@@ -1,11 +1,16 @@
+// src/cat-in-ambush/cat-in-ambush.controller.ts
 import { Controller, Post } from '@nestjs/common';
-// biome-ignore lint/style/useImportType: <explanation>
 import { CatInAmbushService } from './cat-in-ambush.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('ambush')
+@ApiTags('ambush') // コントローラーにタグを追加
 export class CatInAmbushController {
   constructor(private service: CatInAmbushService) {}
+
   @Post()
+  @ApiOperation({ summary: '銀行の取引明細を取得' })
+  @ApiResponse({ status: 200, description: '成功', type: [String] }) // レスポンスの型を指定
   public async ambush() {
     return await this.service.ambush();
   }
